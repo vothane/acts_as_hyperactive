f3bd634757ac547d2fe3c6caaa07c3d836f61f3d
Thane Vo <vothane@gmail.com>
2012-10-18 15:58:39 -0600

    stubbed some tests for TDD

--- spec/acts_as_hyperactive_spec.rb
+++ spec/acts_as_hyperactive_spec.rb
@@ -3,5 +3,19 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe 'acts_as_hyperactive' do 
 
+  context "when included" do
+    
+    it "should modifiy ActiveResource" do
+      ActiveResource::Connection.should be_a( Acts::Hyperactive )
+    end
+    
+    it "should included acts_as_hyperactive alias methods" do
+      ActiveResource::Connection.should respond_to( :get_with_hyperactive )
+      ActiveResource::Connection.should respond_to( :post_with_hyperactive )
+      ActiveResource::Connection.should respond_to( :put_with_hyperactive )
+      ActiveResource::Connection.should respond_to( :delete_with_hyperactive )
+    end
+    
+  end
 	
 end
